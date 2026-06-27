6a862f68e008e706e4dca53f5b1fc401a360c9af
RyanRakey <crusos.denis@gmail.com>
2026-06-27 13:32:45 +0300

    Add tools.build jar/uberjar support

---
+++ build.clj
@@ -0,0 +1,39 @@
+(ns build
+  (:refer-clojure :exclude [compile])
+  (:require [clojure.tools.build.api :as b]))
+
+(def lib 'val-nice-tutorial/val-nice-tutorial)
+(def class-dir "target/classes")
+(def basis (b/create-basis {:project "deps.edn"}))
+(def main-ns 'val-nice-tutorial.core)
+
+(defn clean [_]
+  (b/delete {:path "target"}))
+
+(defn jar [_]
+  (b/write-pom {:class-dir class-dir
+                :lib lib
+                :version "0.1.0"
+                :basis basis
+                :src-dirs ["src"]})
+  (b/copy-dir {:src-dirs ["src"]
+               :target-dir class-dir})
+  (b/jar {:class-dir class-dir
+          :jar-file "target/val-nice-tutorial.jar"}))
+
+(defn uberjar [_]
+  (clean nil)
+  (b/write-pom {:class-dir class-dir
+                :lib lib
+                :version "0.1.0"
+                :basis basis
+                :src-dirs ["src"]})
+  (b/copy-dir {:src-dirs ["src"]
+               :target-dir class-dir})
+  (b/compile-clj {:basis basis
+                  :src-dirs ["src"]
+                  :class-dir class-dir})
+  (b/uber {:class-dir class-dir
+           :uber-file "target/val-nice-tutorial-standalone.jar"
+           :basis basis
+          :main main-ns}))
